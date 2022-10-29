3117d2d7e29002e57c9b5b4bf4eed15aaa458770
yqxyz <quentinyy@gmail.com>
2022-10-30 00:32:16 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -62,7 +62,7 @@ LINKER=ld.lld
 
 ##----------------------------------------------------------##
 # Specify compiler [ proton, atomx, eva, aosp ]
-COMPILER=sdclang
+COMPILER=aosp
 
 ##----------------------------------------------------------##
 # Clone ToolChain
