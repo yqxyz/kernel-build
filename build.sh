3cb02905291cf6b6718e1af1b13285aa71c16768
yqxyz <quentinyy@gmail.com>
2022-10-29 23:44:15 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -114,6 +114,7 @@ function cloneTC() {
 	wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/master/${CLANG_VERSION}.tgz
         tar -xf ${CLANG_VERSION}.tgz
         cd .. || exit
+    git clone https://android.googlesource.com/platform/prebuilts/gas/linux-x86 prebuilts/gas/linux-x86
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git --depth=1 gcc
 	git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git  --depth=1 gcc32
 	fi
@@ -238,7 +239,8 @@ START=$(date +"%s")
 	       ARCH=arm64 \
 	       LLVM=1 \
 	       LLVM_IAS=1 \
-	       CLANG_TRIPLE=aarch64-linux-gnu- \
+	       LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN=prebuilts/gas/linux-x86 \
+           LINUX_GCC_CROSS_COMPILE_COMPAT_PREBUILTS_BIN=prebuilts/gas/linux-x86 \
 	       CROSS_COMPILE=aarch64-linux-android- \
 	       CROSS_COMPILE_COMPAT=arm-linux-androideabi- \
 	       V=$VERBOSE 2>&1 | tee error.log
