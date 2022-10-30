62a2d27ccf8638f1e2b0e810151ee3907fd08e0f
yqxyz <quentinyy@gmail.com>
2022-10-30 13:39:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -238,9 +238,9 @@ START=$(date +"%s")
         elif [ -d ${KERNEL_DIR}/clangB ];
            then
            make O=out CC=clang ARCH=arm64 ${DEFCONFIG}
-          # cp ../pix_docker.config out/.config
-          # cp ../pixMakefile kernel/Makefile
-          # cp ../pixxt_qtaguid.c net/netfilter/xt_qtaguid.c
+           cp ../1pix.config out/.config
+           cp ../1pixMakefile kernel/Makefile
+           cp ../1pixxt_qtaguid.c net/netfilter/xt_qtaguid.c
            make -kj$(nproc --all) O=out \
 	       ARCH=arm64 \
 	       LLVM=1 \
