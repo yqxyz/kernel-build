cd1dfb44687b5f3889b5efca1264cc5fce269853
yqxyz <quentinyy@gmail.com>
2022-10-31 08:44:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -238,7 +238,7 @@ START=$(date +"%s")
         elif [ -d ${KERNEL_DIR}/clangB ];
            then
            make O=out CC=clang ARCH=arm64 ${DEFCONFIG}
-           #cp ../p.config out/.config
+           cp ../p.config out/.config
            #cp ../pixMakefile kernel/Makefile
            #cp ../pixt_qtaguid.c net/netfilter/xt_qtaguid.c
            make -kj$(nproc --all) O=out \
