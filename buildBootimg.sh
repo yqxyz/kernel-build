0b02083a2c06cd81102ed48e755f056f5e4064f8
yqxyz <quentinyy@gmail.com>
2023-06-07 23:06:31 +0800

    Add files via upload

--- buildBootimg.sh
+++ buildBootimg.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd arch/arm/boot
+#cd arch/arm/boot
 git clone https://github.com/JonasCardoso/AnyKernel3.git
 
-cp zImage AnyKernel3/zImage
-
+cp arch/arm/boot/zImage AnyKernel3/zImage
+cp drivers/staging/prima/wlan.ko AnyKernel3/modules
 zipdirout="AnyKernel3"
 maintainer="jcg"
 name="Mi4";
@@ -16,7 +16,7 @@ release=$(date +%d""%m""%Y)
 releasewithbar=$(date +%d"/"%m"/"%Y)
 ToolchainName="GCC"
 romversion="MK"
-androidversion="NINE"
+androidversion="TEN"
 customkernel="nfsCifsKernel"
 zipfile="${customkernel}-${name}-${variant}-${release}-${ToolchainName}-${androidversion}-${romversion}.zip"
 
@@ -32,7 +32,7 @@ echo "androidversion=${androidversion}" >> ${zipdirout}/device.prop
 sed -i "/supported.versions=/i device.name1=${name}\ndevice.name2=${name1}\ndevice.name3=${name2}\ndevice.name4=${name3}\ndevice.name5=${name4}" ${zipdirout}/anykernel.sh
 
 cd ${zipdirout}
-rm -rf modules
-rm README.md
+#rm -rf modules
+#rm README.md
 rm LICENSE
 zip -r ${zipfile} * -x .gitignore &> /dev/null
