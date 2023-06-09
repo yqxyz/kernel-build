4454ecd9bb8df0d7508ef2b14a33312d77d5e2c7
yqxyz <quentinyy@gmail.com>
2023-06-10 01:14:58 +0800

    Add files via upload

--- S0NiX.sh
+++ S0NiX.sh
@@ -142,6 +142,9 @@ START=$(date +"%s")
 	
 	# Compile
 	make O=out CC=clang ARCH=arm64 ${DEFCONFIG}
+    cp ../xt_qtaguid.c net/netfilter
+    cp ../Makefile kernel
+    cp ../_.config out
 	if [ -d ${KERNEL_DIR}/clang ];
 	   then
 	       make -kj$(nproc --all) O=out \
