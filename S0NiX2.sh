e0689ee1a563fb12f22981058ec05ed5dd69325f
yqxyz <quentinyy@gmail.com>
2023-08-04 12:05:01 +0800

    Update S0NiX2.sh

--- S0NiX2.sh
+++ S0NiX2.sh
@@ -141,7 +141,7 @@ START=$(date +"%s")
 	post_msg "<b>$KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Docker OS: </b><code>$DISTRO</code>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Kolkata date)</code>%0A<b>Device : </b><code>$MODEL [$DEVICE]</code>%0A<b>Pipeline Host : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>"
 	
 	# Compile
- 	cp ../wlan_extscan_api.c drivers/staging/qca-wifi-host-cmn/umac/scan/dispatcher/src
+ 	#cp ../wlan_extscan_api.c drivers/staging/qca-wifi-host-cmn/umac/scan/dispatcher/src
 	make O=out CC=clang ARCH=arm64 ${DEFCONFIG}
         #cp ../xt_qtaguid.c net/netfilter
         #cp ../Makefile kernel
