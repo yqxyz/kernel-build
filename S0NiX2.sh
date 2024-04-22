5b20e302b8cab0b53b3269fa796f55cdae38b720
yqxyz <quentinyy@gmail.com>
2024-04-22 11:40:44 +0800

    Update S0NiX2.sh

--- S0NiX2.sh
+++ S0NiX2.sh
@@ -40,7 +40,7 @@ FINAL_ZIP=${ZIPNAME}-${VERSION}-${DEVICE}-${TANGGAL}.zip
 
 ##----------------------------------------------------------##
 # Specify compiler - nexus, proton, azure, aosp
-COMPILER=azure
+COMPILER=aosp
 
 ##----------------------------------------------------------##
 # Clone ToolChain
